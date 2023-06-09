02c5bd0c932e184e5dbcdbcca6e2caacd5731543
Vaibhav2157 <92290672+Vaibhav2157@users.noreply.github.com>
2023-06-09 15:17:08 +0530

    sidebar dropdowns

--- src/Pages/College/Dashboard.jsx
+++ src/Pages/College/Dashboard.jsx
@@ -8,27 +8,27 @@ import StudentData from '../../components/StudentData'
 function Dashboard() {
   return (
     <div className='bg-white flex'>
-        <div className='hidden sm:flex'>
-    <Sidebar/>
-    </div>
-    <div className='flex-[3]'>
-    
-      <Navbar/>
-      <hr className='h-0 border-r-[0.5px] border-solid border-[#E6E3E3]'/>
-   <StudentData/>
-    {/* <div className='flex flex-col sm:flex-row p-[20px] gap-[20px]'>
+      <div className='hidden sm:flex'>
+        <Sidebar />
+      </div>
+      <div className='flex-[3]'>
+
+        <Navbar />
+        <hr className='h-0 border-r-[0.5px] border-solid border-[#E6E3E3]' />
+        <StudentData />
+        {/* <div className='flex flex-col sm:flex-row p-[20px] gap-[20px]'>
     <Card type='user'/>
     <Card type='order'/>
     <Card type='earning'/>
     <Card type='balance'/>
     </div> */}
-    
-    </div>
-    
 
-    
-    
-   </div>
+      </div>
+
+
+
+
+    </div>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,32 @@
 import React, { useState } from 'react'
 import { Link } from "react-router-dom"
+import ArrowDropDownRoundedIcon from '@mui/icons-material/ArrowDropDownRounded';
 
 function Sidebar() {
 
-  const [show, setshow] = useState(false)
+  const [showCompany, setshowCompany] = useState(false)
+  const [showCalendar, setshowCalendar] = useState(false)
+
 
   return (
     <div className="flex flex-col w-[200px] border-r-[0.5px] border-solid border-[#E6E3E3] bg-white min-h-screen ">
       <div className="h-[50px] flex items-center justify-center">
         <Link to="/" style={{ textDecoration: "none" }}>
-        <span className="text-xl font-bold text-[#6439ff]">Admin</span>
+          <span className="text-xl font-bold text-[#6439ff]">Admin</span>
         </Link>
         {/* <span className="text-xl font-bold text-[#6439ff]">Admin</span> */}
       </div>
-      <hr className='h-0 border-r-[0.5px] border-solid border-[#E6E3E3]'/>
+      <hr className='h-0 border-r-[0.5px] border-solid border-[#E6E3E3]' />
       <div className="pl-[10px]">
         <ul className='m-0 p-0 list-none'>
           <p className="text-[10px] font-bold text-[#999] mt-[15px] mb-[5px]">MAIN</p>
           <Link to="/" style={{ textDecoration: "none" }}>
-          <li className='flex items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
-            {/* <DashboardIcon className="icon" /> */}
-            <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Dashboard</span>
-          </li>
+            <li className='flex items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
+              {/* <DashboardIcon className="icon" /> */}
+              <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Dashboard</span>
+            </li>
           </Link>
-          
+
           <p className="text-[10px] font-bold text-[#999] mt-[15px] mb-[5px]">LISTS</p>
           <Link to="/users" style={{ textDecoration: "none" }}>
             <li className='flex items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
@@ -31,29 +34,67 @@ function Sidebar() {
               <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Students</span>
             </li>
           </Link>
-          
-          <Link to="/hotels" style={{ textDecoration: "none" }}>
-          <li className='items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
-              {/* <StoreIcon className="icon" />  */}
-              <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Companies</span>
-              {show && <div className="flex flex-col">
-                <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Subscribed Companies</span> 
-                <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Confirmed Companies</span> 
-              </div>}
-              
-            </li>
-          </Link>
-          
+
+
+          <li className='items-center p-[5px] cursor-pointer'>
+            {/* <StoreIcon className="icon" />  */}
+            <div onClick={()=>{setshowCompany(!showCompany)}}>
+            <span className='text-[13px] mr-2 font-semibold text-[#888] ml-[10px]'>Companies</span>
+            <ArrowDropDownRoundedIcon style={{color:"grey"}}/>
+            </div>
+            
+            {showCompany &&
+              <ul>
+                <div className="flex flex-col">
+                  <li className='items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
+                    <Link to="/subscribedCompanies" style={{ textDecoration: "none" }}>
+                      <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Subscribed Companies</span>
+                    </Link>
+                  </li>
+                  <li className='items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
+                    <Link to="/confirmedCompanies" style={{ textDecoration: "none" }}>
+                      <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Confirmed Companies</span>
+                    </Link>
+                  </li>
+                </div>
+              </ul>
+            }
+
+          </li>
+
+
           <Link to="/rooms" style={{ textDecoration: "none" }}>
-          <li className='flex items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
+            <li className='flex items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
               {/* <CreditCardIcon className="icon" /> */}
               <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Departments</span>
             </li>
           </Link>
-          
-          <li className='flex items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
-            {/* <LocalShippingIcon className="icon" /> */}
-            <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Calender</span>
+
+
+          <li className='items-center p-[5px] cursor-pointer'>
+            {/* <StoreIcon className="icon" />  */}
+            <div onClick={()=>setshowCalendar(!showCalendar)}>
+            <span className='text-[13px] mr-2 font-semibold text-[#888] ml-[10px]'>Calendar</span>
+            <ArrowDropDownRoundedIcon style={{color:"grey"}}/>
+            </div>
+            
+            {showCalendar &&
+              <ul>
+                <div className="flex flex-col">
+                  <li className='items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
+                    <Link to="/subscribedCompanies" style={{ textDecoration: "none" }}>
+                      <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Publish Calendar</span>
+                    </Link>
+                  </li>
+                  <li className='items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
+                    <Link to="/confirmedCompanies" style={{ textDecoration: "none" }}>
+                      <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Display Calendar</span>
+                    </Link>
+                  </li>
+                </div>
+              </ul>
+            }
+
           </li>
 
           <p className="text-[10px] font-bold text-[#999] mt-[15px] mb-[5px]">USER</p>
