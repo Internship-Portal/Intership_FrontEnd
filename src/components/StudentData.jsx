4bf8346135eff6de2cf1eb78736af1d44b74f03d
Vaibhav2157 <92290672+Vaibhav2157@users.noreply.github.com>
2023-06-09 13:35:44 +0530

    Student table commit

--- src/components/StudentData.jsx
+++ src/components/StudentData.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
 import MUIDataTable from 'mui-datatables'
-const StudentData = () => {
-    const columns = ["Name", "Company", "City", "State"];
 
-    const data = [
-        ["Joe James", "Test Corp", "Yonkers", "NY"],
-        ["John Walsh", "Test Corp", "Hartford", "CT"],
-        ["Bob Herm", "Test Corp", "Tampa", "FL"],
-        ["James Houston", "Test Corp", "Dallas", "TX"],
-       ];
-       
-       const options = {
-         filterType: 'checkbox',
-       };
-       
+const StudentData = () => {
+  const columns = ["Name", "Email Id", "Mobile No.", "Roll No.", "Skills", "Achievments", "CGPA", "Unavailable Dates", "Internship Status", "CGPA", "CGPA", "CGPA"];
+
+  const data = [
+    ["Vaibhav desale", "vaibhav.desale20@pccoepune.org", "7875543720", "TYITA27", "Full Stack", "NO", "8.3", "1- June - 31-July", "Yes"],
+    ["John Walsh", "Test Corp", "Hartford", "CT"],
+    ["Bob Herm", "Test Corp", "Tampa", "FL"],
+    ["James Houston", "Test Corp", "Dallas", "TX"],
+  ];
+
+  const options = {
+    filterType: 'checkbox',
+  };
+
 
   return (
-    <MUIDataTable
-    title={"Employee List"}
-    data={data}
-    columns={columns}
-    options={options}
-  />
+      <MUIDataTable
+        title={"Students"}
+        data={data}
+        columns={columns}
+        options={options}
+      />
+
   )
 }
 
