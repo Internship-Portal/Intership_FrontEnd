6e00caf834d001fc859df773a1fee34abb264799
Vaibhav2157 <92290672+Vaibhav2157@users.noreply.github.com>
2023-06-09 11:31:52 +0530

    sidebar third commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, { useState } from 'react'
+import { Link } from "react-router-dom"
 
 function Sidebar() {
+
+  const [show, setshow] = useState(false)
+
   return (
     <div className="flex-1 border-r-[0.5px] border-solid border-[#E6E3E3] bg-white min-h-screen">
       <div className="h-[50px] flex items-center justify-center">
@@ -25,27 +28,32 @@ function Sidebar() {
           <Link to="/users" style={{ textDecoration: "none" }}>
             <li className='flex items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
               {/* <PersonOutlineIcon className="icon" /> */}
-              <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Users</span>
+              <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Students</span>
             </li>
           </Link>
           
           <Link to="/hotels" style={{ textDecoration: "none" }}>
-          <li className='flex items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
+          <li className='items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
               {/* <StoreIcon className="icon" />  */}
-              <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Hotels</span>
+              <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Companies</span>
+              {show && <div className="flex flex-col">
+                <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Subscribed Companies</span> 
+                <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Confirmed Companies</span> 
+              </div>}
+              
             </li>
           </Link>
           
           <Link to="/rooms" style={{ textDecoration: "none" }}>
           <li className='flex items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
               {/* <CreditCardIcon className="icon" /> */}
-              <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Rooms</span>
+              <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Departments</span>
             </li>
           </Link>
           
           <li className='flex items-center p-[5px] cursor-pointer hover:bg-[#ece8ff]'>
             {/* <LocalShippingIcon className="icon" /> */}
-            <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Delivery</span>
+            <span className='text-[13px] font-semibold text-[#888] ml-[10px]'>Calender</span>
           </li>
 
           <p className="text-[10px] font-bold text-[#999] mt-[15px] mb-[5px]">USER</p>
