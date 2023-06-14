56bda8dd756a7486737dbba5a973cbf1164969a1
Arnav Deshmukh <arnav.deshmukh20@pccoepune.org>
2023-06-15 01:49:21 +0530

    login page updated

--- src/Pages/College/Login.jsx
+++ src/Pages/College/Login.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import img from "..//../assets/login_2.gif";
+import { launchPoster } from "../../assets";
 const Login = (props) => {
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
         <div className=" hidden sm:block">
-          <div className="w-full h-screen object-cover bg-fuchsia-200"   >
-            <img className=" z-1 h-screen p-32 mx-auto flex items-center justify-center" src={img} alt="" />
+          <div className="w-full h-screen object-cover bg-primary opacity-80"   >
+            <img className=" z-1 h-screen p-32 mx-auto flex items-center justify-center" src={launchPoster} alt="" />
           </div>
           
         </div>
 
         <div className=" flex flex-col justify-center ">
-          <form className="max-w-[380px] w-full mx-auto bg-fuchsia-200 p-8 px-8 rounded-3xl">
-              <h2 className=" text-4xl dark:text-black font-bold text-center">
+          <form className="max-w-[380px] w-4/5 sm:w-full mx-auto bg-primary opacity-80 p-8 px-8 rounded-3xl">
+              <h2 className=" text-4xl dark:text-white font-poppins font-bold text-center">
                 Login
               </h2>
-              <h4 className=" my-2 mt-4 text-gray-800 text-center">
+              <h4 className=" my-2 mt-4 text-gray-900 text-center">
                 Welcome {props.type}
               </h4>
             <div className="max-w-[300px] mx-auto w-full  bg-white p-8 px-8  rounded-3xl ">
-              <div className="flex flex-col text-fuchsia-600 py-2">
+              <div className="flex flex-col text-primary opacity-80 py-2">
                 <label> Email</label>
                 <input
                   type="text"
@@ -28,18 +28,18 @@ const Login = (props) => {
                   className=" rounded-lg bg mt-2 p-2 focus:bottom-1 border-b-2 border-gray-700"
                 ></input>
               </div>
-              <div className="flex flex-col text-fuchsia-600  py-2">
+              <div className="flex flex-col text-primary opacity-80 py-2">
                 <label>Password</label>
                 <input
                   type="password"
                   placeholder="Password"
-                  className=" rounded-lg bg mt-2 p-2 focus:bottom-1 border-b-2  border-gray-700"
+                  className=" rounded-lg bg mt-2 p-2 focus:bottom-1 border-b-2  border-gray-800 text-black"
                 ></input>
               </div>
-              <button className=" my-2 py-1 bg-fuchsia-400 w-full rounded-xl text-xl">
+              <button className=" my-2 py-1 bg-primary text-black w-full rounded-xl text-xl">
                 Login
               </button>
-              <div className=" text-center text-fuchsia-600">
+              <div className="text-center text-primary opacity-80">
                 <p>Forgot Password ?</p>
               </div>
             </div>
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,8 @@
 import menu from './menu.svg'
 import close from './close.svg'
+import launchPoster from './launchPoster.png'
 export{
-  
+     launchPoster,
     close,
     menu
 };
