daa8086aa506f3f84a707662738b40a0851ed317
Arnav Deshmukh <arnav.deshmukh20@pccoepune.org>
2023-06-15 12:19:08 +0530

    Upload list updated

--- src/components/UploadList.jsx
+++ src/components/UploadList.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
 
 const UploadList = () => {
+    const [selectedFile, setSelectedFile] = useState();
+	const [isFilePicked, setIsFilePicked] = useState(false);
+
+	const changeHandler = (event) => {
+		setSelectedFile(event.target.files[0]);
+		setIsSelected(true);
+	};
+
+	const handleSubmission = () => {
+        console.log(selectedFile)
+	};
   return (
     <div  className='flex flex-col items-center text-center sm:text-left mt-6'>
         
@@ -11,7 +23,7 @@ const UploadList = () => {
             <p class="mb-2 text-sm text-black "><span class="font-semibold">Click to upload</span> or drag and drop</p>
             <p class="text-xs text-black">CSV</p>
         </div>
-        <input id="dropzone-file" type="file" class="hidden" />
+        <input id="dropzone-file" type="file" class="hidden" onChange={changeHandler} />
     </label>
 </div> 
 
@@ -20,12 +32,12 @@ const UploadList = () => {
 <div className='w-3/4'>
   
 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
-<input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file"/>
+<input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file" onChange={handleSubmission}/>
 <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
  
 </div>
 
-<button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+<button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" onChange={handleSubmission}>
   <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
   <span>Upload</span>
 </button>
