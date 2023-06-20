351432a5d027288f573df4b1b2b5b8c498036239
Vaibhav2157 <92290672+Vaibhav2157@users.noreply.github.com>
2023-06-20 14:31:26 +0530

    display students commit

--- src/components/StudentData.jsx
+++ src/components/StudentData.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useEffect , useContext , useState } from "react";
+import axios from "axios";
 import MUIDataTable from "mui-datatables";
 import { MuiThemeProvider } from "@material-ui/core";
 import "../components/StudentData.css";
@@ -7,8 +7,16 @@ import Dropdown_batch from "./Dropdown_batch";
 import Dropdown_dept from "./Dropdown_dept";
 import useFetch from "../hooks/useFetch";
 import { AuthContext } from "../context/AuthContext";
+import config from "../hooks/config";
 
 const StudentData = () => {
+
+  const [student ,setStudent] = useState({
+    department_name:"",
+    year_batch:null,
+    students:[]
+  })
+
   const columns = [
     "name",
     "email_id",
@@ -25,22 +33,47 @@ const StudentData = () => {
     "twelve_percentage",
   ];
 
-  // const { loading, error, dispatch } = useContext(AuthContext);
-  const { data, loading, error } = useContext(AuthContext);
-  // console.log(data)
+  const {loading, error } = useContext(AuthContext);
+  const {id , headers} = config()
+ 
   const options = {
     filterType: 'checkbox',
     responsive: "scroll",
   };
 
+  const handleBatch =(data)=>{
+    setStudent({...student, year_batch:data })
+  }
+
+  const handleDept =(data)=>{
+    setStudent({...student, department_name:data })
+  }
+
+  useEffect( ()=>{
+    const fetchStudent = async ()=>{
+      try {
+        const res = await axios.post(`http://localhost:4000/api/officer/getStudentDetails/${id}`,student, { headers } )
+        setStudent({...student,students : res.data.data.student_details})
+      } catch (error) {
+        console.log(error.response.data)
+      }
+    }
+    fetchStudent()
+  },[student.year_batch,student.department_name]);
+
   return (
     <div>
+      <div className="flex">
+      <Dropdown_batch onhandleBatchChange={handleBatch}/>
+      <Dropdown_dept onhandleDeptChange={handleDept}/>
+      </div>
+      
       {loading ? ("Loading") : (
         <div style={{ display: 'table', tableLayout: 'fixed', width: '100%' }}>
           <MuiThemeProvider >
             <MUIDataTable
               title={"Students"}
-              data={data}
+              data={student.students}
               columns={columns}
               options={options}
             />
