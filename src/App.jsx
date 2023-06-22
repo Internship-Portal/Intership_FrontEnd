1a75776e464461c0a0bce3cb65e5a7dcf1757dd3
Vaibhav2157 <92290672+Vaibhav2157@users.noreply.github.com>
2023-06-22 14:12:45 +0530

    company dashboard created

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import OTP from './Pages/Login/OTP'
 import SignUp from './Pages/Login/SignUp'
 import SubComp from './Pages/College/SubComp'
 import ConfComp from './Pages/College/ConfComp'
+import DashBoard from './Pages/company/DashBoard'
 import GetCompany from './Pages/company/GetCollege'
 
 function App() {
@@ -16,8 +17,9 @@ function App() {
    <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Dashboard/>}></Route>
-          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/officer' element={<Dashboard/>}></Route>
+          <Route path='/company' element={<DashBoard/>}></Route>
+          <Route path='/' element={<Login/>}></Route>
           <Route path='/addstudents' element={<AddStudent/>}></Route>
           <Route path='/displaystudents' element={<DisplayStudent/>}></Route>
           <Route path='/subscribedCompanies' element={<SubComp/>}></Route>
---
+++ src/Pages/company/DashBoard.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect} from 'react'
+import Navbar from '../../components/Navbar'
+import Sidebar from '../../components/Sidebar'
+import config from '../../hooks/config'
+// import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function Dashboard() {
+  const {id} =config()
+
+  useEffect(() => {
+    console.log(id)
+  }, []);
+
+  return (
+    <div className='bg-white flex'>
+      <div className='hidden sm:flex'>
+        <Sidebar />
+      </div>
+      <div className='flex-[3]'>
+        <Navbar />
+        <hr className='h-0 border-r-[0.5px] border-solid border-[#E6E3E3]' />
+      </div>
+      {/* <ToastContainer/> */}
+    </div>
+  )
+}
+
+export default Dashboard
