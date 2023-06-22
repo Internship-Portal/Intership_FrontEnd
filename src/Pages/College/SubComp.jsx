04e0d31c8449656903667cd7cabcfe615ad4bd32
DeOkumar Manwar <86521201+DeokumarManwar@users.noreply.github.com>
2023-06-22 16:26:02 +0530

    Added fetch method

--- src/Pages/College/SubComp.jsx
+++ src/Pages/College/SubComp.jsx
@@ -1,70 +1,81 @@
-import React, { useContext, useState } from 'react'
-import axios from 'axios'
-import Sidebar from '../../components/Sidebar'
-import Navbar from '../../components/Navbar'
-import { AuthContext } from '../../context/AuthContext'
-import config from '../../hooks/config'
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import Sidebar from "../../components/Sidebar";
+import Navbar from "../../components/Navbar";
+import { AuthContext } from "../../context/AuthContext";
+import config from "../../hooks/config";
 
 function SubComp() {
+  const [comp, setComp] = useState();
 
-  const [comp,setComp]=useState({
-    company_id :"",
-    message :""
-  })
+  const { data } = useContext(AuthContext);
+  console.log(data);
 
-  const { data } = useContext(AuthContext)
-  console.log(data)
-
-  const { headers } = config()
+  const { headers } = config();
 
   const handleClick = async (company) => {
+    const data = {
+      company_id: company.company_id,
+      message: company.message,
+    };
 
-    setComp({
-      company_id:company.company_id,
-      message:company.message
-    })
+    const headers = {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiNjQ4OWM1MTdkMDVmMjdjY2MwY2ZjMDkxIiwiZW1haWxfaWQiOiJkdW1teW9mZmljZXJAZ21haWwuY29tIiwiaWF0IjoxNjg3MjAxNjQ3fQ.ZtK7GpIkmjd7uDOCy7eobfQ55ibUwh0Z0kYU6AaA6Zg",
+      "Content-Type": "application/json",
+    };
 
     try {
-      console.log(comp)
-      console.log(headers)
-      const res = await axios.put("http://localhost:4000/api/officer/addSubscribedOfficerFromOfficer", comp , { headers })
-      console.log(res)
+      const response = await fetch(
+        "http://localhost:4000/api/officer/addSubscribedOfficerFromOfficer",
+        {
+          method: "PUT",
+          headers: headers,
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+
+      const result = await response.json();
+      console.log(result);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
-    <div className='bg-white flex'>
-      <div className='hidden sm:flex'>
+    <div className="bg-white flex">
+      <div className="hidden sm:flex">
         <Sidebar />
       </div>
-      <div className='flex-[3]'>
+      <div className="flex-[3]">
         <Navbar />
-        <hr className='h-0 border-r-[0.5px] border-solid border-[#E6E3E3]' />
+        <hr className="h-0 border-r-[0.5px] border-solid border-[#E6E3E3]" />
 
-        {
-          data && data.map((company) => (
+        {data &&
+          data.map((company) => (
             <div
               className=" text-center items-center inline-flex w-full sm:w-1/3"
               key={company.index}
             >
               <div className="border-solid border-black border-2 w-3/4 mx-auto my-2 ">
                 <div className=" ">
-                  <div className=" text-lg font-bold justify-start flex px-4">{company.company_name}</div>
+                  <div className=" text-lg font-bold justify-start flex px-4">
+                    {company.company_name}
+                  </div>
                   <div className="">{company.company_id}</div>
                   <div>{company.message}</div>
                 </div>
-                <button onClick={()=>handleClick(company)}>Accept</button>
+                <button onClick={() => handleClick(company)}>Accept</button>
               </div>
             </div>
-          ))
-        }
-
+          ))}
       </div>
-
     </div>
-  )
+  );
 }
 
-export default SubComp
+export default SubComp;
